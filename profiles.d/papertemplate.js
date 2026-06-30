4709beff4e021ada240d59c130e1bc4ccfd1f278
ggee <gee.developer@gmail.com>
2026-06-29 22:01:16 -0400

    Update Java 25 and PaperMC v3 profiles

--- profiles.d/papertemplate.js
+++ profiles.d/papertemplate.js
@@ -6,18 +6,36 @@ const Profile = require('./template'); // your Profile class
 module.exports = function papertemplate(name) {
   const lowername = name.toLowerCase();
   const titlename = name.charAt(0).toUpperCase() + lowername.slice(1);
+  const versionType = version => /-(pre|rc)-?\d*$/i.test(version) ? 'snapshot' : 'release';
 
   return {
     name: titlename,
     request_args: {
-      url: `https://api.papermc.io/v2/projects/${lowername}`,
+      url: `https://fill.papermc.io/v3/projects/${lowername}`,
       json: true
     },
 
     handler: function(profile_dir, body, callback) {
       try {
-        const promises = body.versions.map(version => {
-          return axios({ url: `https://api.papermc.io/v2/projects/${lowername}/versions/${version}/` })
+        const versions = Object.values(body.versions || {}).reduce((all, versionGroup) => {
+          return all.concat(versionGroup);
+        }, []);
+
+        const promises = versions.map(version => {
+          return axios({ url: `https://fill.papermc.io/v3/projects/${lowername}/versions/${version}` })
+            .then(response => {
+              if (!response.data.builds || response.data.builds.length === 0) return null;
+
+              const build = response.data.builds[0];
+              return axios({
+                url: `https://fill.papermc.io/v3/projects/${lowername}/versions/${version}/builds/${build}`
+              }).then(buildResponse => ({
+                build,
+                version,
+                time: buildResponse.data.time,
+                download: buildResponse.data.downloads && buildResponse.data.downloads['server:default']
+              }));
+            })
             .catch(err => {
               if (err.response && err.response.status === 404) {
                 console.warn(`Version ${version} not found (404). Skipping.`);
@@ -35,25 +53,25 @@ module.exports = function papertemplate(name) {
 
           results.forEach(result => {
             if (result.status === 'fulfilled' && result.value) {
-              const response = result.value;
-
-              if (!response.data.builds || response.data.builds.length === 0) return;
-
-              const build = response.data.builds[response.data.builds.length - 1];
-              const splitPath = response.request.path.split('/');
-              const ver = splitPath[splitPath.length - 2];
+              const buildInfo = result.value;
+              const build = buildInfo.build;
+              const ver = buildInfo.version;
+              const download = buildInfo.download;
+              if (!download || !download.name || !download.url) return;
 
               const item = new Profile();
               item.id = `${titlename}-${ver}-${build}`;
               item.group = lowername;
               item.webui_desc = `Latest ${titlename} build for ${ver}`;
-              item.weight = weight;
-              item.filename = `${lowername}-${ver}-${build}.jar`;
-              item.url = `${response.request.res.responseUrl}builds/${build}/downloads/${lowername}-${ver}-${build}.jar`;
+              item.weight = versions.length - weight;
+              item.filename = download.name;
+              item.url = download.url;
               item.downloaded = fs.existsSync(path.join(profile_dir, item.id, item.filename));
+              item.time = buildInfo.time;
+              item.releaseTime = buildInfo.time;
               item.version = ver;
               item.release_version = ver;
-              item.type = 'release';
+              item.type = versionType(ver);
 
               items.push(item);
               weight++;
